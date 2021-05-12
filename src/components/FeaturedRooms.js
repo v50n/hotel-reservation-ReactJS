e3cd4844d1344e17f2de9623a25d8bef7d6e7dd2
Viet Son <vson.ng91@gmail.com>
2021-05-12 15:53:45 +0200

    add css to finish the featured Rooms

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import data from './../data';
+import {Link} from 'react-router-dom'
 
 export default function FeaturedRooms() {
     var featuredRoom = data.filter(room => room.fields.featured === true);
@@ -13,13 +14,17 @@ export default function FeaturedRooms() {
                     featuredRoom.map((room,index) =>{
                         return (
                             <div className="roomDetail" key={index}>
-                                <div className="roomPrice">
-                                    ${room.fields.price}
-                                    <div className="night"><span>per night</span></div>
-                                </div>
+
                                 <div className="roomImage">
-                                    <img src={room.fields.images[0].fields.file.url} alt="" />
-                                    <button className="btn-features">Features</button>
+                                    <img src={room.fields.images[0].fields.file.url} alt="image" />
+                                    <div className="roomPrice">
+                                    ${room.fields.price}
+                                    <p className="night"><span>per night</span></p>
+                                    </div>
+                                    
+                                        <Link to={"/rooms/" + room.fields.slug}className="btn-features">Features</Link>
+                                    
+
                                 </div>
                                 <div className="roomName">
                                     <h2>{room.fields.name}</h2>

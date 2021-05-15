6a2853c3895cfd458a57a5b2e95d71d2c23787c3
Viet Son <vson.ng91@gmail.com>
2021-05-15 16:41:44 +0200

    Start to edit the room page

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import StyledHero from '../components/StyledHero';
+import HeroBanner from '../components/HeroBanner';
 
 const Rooms = () => {
     return (
         <div>
-            Rooms page
+            <StyledHero>
+                <HeroBanner title="Our rooms" btnTitle="Return to rooms"></HeroBanner>
+            </StyledHero>
+            <section>
+                <form action="">
+                    <h1>
+                        Search Rooms
+                    </h1>
+                    <select name="" id="selectRoom">
+                        <option value="all">all</option>
+                    </select>
+                </form>
+            </section>
         </div>
     )
 }
